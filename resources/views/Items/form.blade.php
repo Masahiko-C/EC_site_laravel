e1145bf6b868550316ff2d9bfe3d8b7e159d291d
Masahiko Naruse <kirakiramasahiko@gmail.com>
2021-05-17 10:06:01 +0900

    フォームに軽微な修正を加えました

--- resources/views/Items/form.blade.php
+++ resources/views/Items/form.blade.php
@@ -1,18 +1,18 @@
     <div class="form-group">
-      {{!! Form::label('name', '名前:') !!}}
-      {{!! Form::text('name', null, ['class' => 'form-controll']) !!}}
+      {!! Form::label('name', '名前:') !!}
+      {!! Form::text('name', null, ['class' => 'form-controll']) !!}
     </div>
     <div class="form-group">
-      {{!! Form::label('price', '価格:') !!}}
-      {{!! Form::number('price', null, ['class' => 'form-controll']) !!}}
+      {!! Form::label('price', '価格:') !!}
+      {!! Form::number('price', null, ['class' => 'form-controll']) !!}
     </div>
     <div class="form-group">
-      {{!! Form::label('stock', '在庫数:') !!}}
-      {{!! Form::text('stock', null, ['class' => 'form-controll']) !!}}
+      {!! Form::label('stock', '在庫数:') !!}
+      {!! Form::text('stock', null, ['class' => 'form-controll']) !!}
     </div>
     <div class="form-group">
-      {{!! Form::label('image', '商品画像:') !!}}
-      {{!! Form::file('image') !!}}
+      {!! Form::label('image', '商品画像:') !!}
+      {!! Form::file('image') !!}
     </div>
     <div class="form-group">
       {!! Form::label('published_at', 'Publish On:') !!}
