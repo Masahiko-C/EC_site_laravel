a90b078bbaa5b335ffeb5d77b37bd3e14c5579ed
Masahiko Naruse <kirakiramasahiko@gmail.com>
2021-07-05 14:41:06 +0900

    test

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -29,7 +29,13 @@
             <td>{{ $item->price }}円</td>
             <td>
             {!! Form::model($item, ['method' => 'PATCH', 'route' => ['admin.update', $item->item_id]]) !!}
-              @include('Admins.form_update', ['submitButtun' => '更新する'])
+              <div class="form-group">
+                {!! Form::label('stock', '在庫数:') !!}
+                {!! Form::text('stock', null, ['class' => 'form-controll']) !!}
+              </div>
+              <div class="form-group">
+                {!! Form::submit($submitButtun, ['class' => 'btn btn-primary form-control']) !!}
+              </div>
             {!! Form::close() !!}
             </td>
             <td>
